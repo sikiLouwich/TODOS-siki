2c5447994b8f241223460b682924b84f310a00e7
siki louwich <siki.l@masterschool.com>
2025-01-02 11:17:05 +0200

    server: create server.
    Create server and serve todos index.html.

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('dist'));
+
+app.get('/todos', (_req, res) => {
+    res.sendFile(path.join(process.cwd(), 'dist/index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
