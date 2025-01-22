b59c0b77bd4639a71e8c452220d12ec242ca74f3
sikilouwich <amasa.louwich@mail.huji.ac.il>
2025-01-22 15:00:24 +0200

    server: use env and enable core.

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,25 @@
-
+import cors from 'cors';
 import express from 'express';
 import path from 'path';
+import dotenv from 'dotenv';
 
-import { TodoService } from './services/todoService';
 import { TodoController } from './controllers/todoController';
 import { createTodoRouter } from './routes/todoRoutes';
+import { TodoService } from './services/todoService';
+
+dotenv.config()
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
+
+// Add CORS middleware before other middleware
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type'],
+  })
+);
 
 //Middleware
 app.use(express.json());
@@ -16,15 +28,15 @@ app.use(express.static('dist'));
 // Initialize services and controllers
 const todoService = new TodoService();
 const todoController = new TodoController(todoService);
-const todoRouter = createTodoRouter(todoController)
+const todoRouter = createTodoRouter(todoController);
 
 // Routes
-app.use("/api/todos", todoRouter);
+app.use('/api/todos', todoRouter);
 
 app.get('*', (_req, res) => {
-    res.sendFile(path.join(process.cwd(), 'dist/index.html'));
+  res.sendFile(path.join(process.cwd(), 'dist/index.html'));
 });
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost: ${port}`);
+  console.log(`Server is running at http://localhost: ${port}`);
 });
